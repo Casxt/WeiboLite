cbc2df768a48c5693ea7be5bcc8b6d8071e8bd35
casxt <774714620@qq.com>
2018-08-29 13:27:43 +0800

    性能测试sql

--- database/TestTool/Top10.sql
+++ database/TestTool/Top10.sql
@@ -1,1 +1,2 @@
+SELECT  (SELECT COUNT(*) FROM "weibo") AS "weibo_count", COUNT(*) AS "comment_count" FROM "comment";
 SELECT "weibo_id" ,COUNT(*) AS "count" FROM "comment" GROUP BY "weibo_id" ORDER BY "count" DESC LIMIT 10;
