619feb6868e9ee77647b161c51a3ada3f0762eb9
casxt <774714620@qq.com>
2018-08-29 18:52:01 +0800

    TOP10获取测试

--- database/TestTool/Top10.sql
+++ database/TestTool/Top10.sql
@@ -1,7 +1,11 @@
 SELECT  (SELECT COUNT(*) FROM "weibo") AS "weibo_count", COUNT(*) AS "comment_count" FROM "comment";
 SELECT "weibo_id" ,COUNT(*) AS "count" FROM "comment" GROUP BY "weibo_id" ORDER BY "count" DESC LIMIT 10;
 
-/*
-给主键加索引之后删除更快了？
-https://dba.stackexchange.com/questions/37034/very-slow-delete-in-postgresql-workaround
-*/
+/*填充comment_count*/
+
+UPDATE weibo2 SET comment_count = COALESCE((SELECT COUNT(*) FROM comment GROUP BY weibo_id HAVING comment.weibo_id = weibo2.id),0);
+
+CREATE INDEX "weibo_comment_count_index" ON "public"."weibo2" USING hash (
+  "comment_count"
+);
+
